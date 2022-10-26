5f6158e03c16487690099d3ff5ab9bef5f1acb28
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-10-26 16:10:21 -0400

    comment ip address

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var SERVER_NAME = "smarthealth";
 var http = require("http");
 var mongoose = require("mongoose");
 
-var port = process.env.PORT;
-var ipaddress = process.env.IP; // Must be changed to integrate heroku later.
+var port = process.env.PORT || 5000;
+// var ipaddress = process.env.IP; // Must be changed to integrate heroku later.
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
@@ -46,21 +46,21 @@ var Patient = mongoose.model("patients", patientSchema);
 var errors = require("restify-errors");
 var restify = require("restify"),
   // Create the restify server
-  server = restify.createServer({ name: SERVER_NAME });
+  server = restify.createServer();
 
-if (typeof ipaddress === "undefined") {
-  //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-  //  allows us to run/test the app locally.
-  console.warn("No process.env.IP var, using default: " + DEFAULT_HOST);
-  ipaddress = DEFAULT_HOST;
-}
+// if (typeof ipaddress === "undefined") {
+//   //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+//   //  allows us to run/test the app locally.
+//   console.warn("No process.env.IP var, using default: " + DEFAULT_HOST);
+//   ipaddress = DEFAULT_HOST;
+// }
 
 if (typeof port === "undefined") {
   console.warn("No process.env.PORT var, using default port: " + DEFAULT_PORT);
   port = DEFAULT_PORT;
 }
 
-server.listen(port, ipaddress, function () {
+server.listen(port, function () {
   console.log("Server %s listening at %s", server.name, server.url);
   console.log("Resources:");
   console.log(" /patients");
