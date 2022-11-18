2288b155c115f86884622977b6d7c9ec9aa609cc
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-11-18 14:14:30 -0500

    mocha chai http test heroku server test file added to folder tests with comments

---
+++ tests/heroku-server-test.js
@@ -0,0 +1,26 @@
+// This is testing if heroku responds status 200
+// On terminal type cd tests then
+// Use this command "mocha heroku-server-test.js" on the terminal to run this test.
+
+var chai = require("chai"),
+  chaiHttp = require("chai-http");
+
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+// Url deployed on heroku
+var url = "https://smarthealth2.herokuapp.com";
+
+//Check if heroku server is running.
+describe("when we issue a 'GET' to heroku server smarthealth2'", function () {
+  it("should return HTTP 200", function (done) {
+    chai
+      .request(url)
+      .get("/")
+      .end(function (req, res) {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+});
